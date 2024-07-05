2a198b1812e2d1f639c8565139dad17476ef1c45
idmaksim <i@dmaksim.ru>
2024-07-05 17:28:38 +0300

    add half of base code

---
+++ fastapi-base/samples/app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+# define your schemas here
+class ItemBase(BaseModel):
+    ...
+
+class ItemCreate(ItemBase):
+    ...
+
+class ItemGet(ItemBase):
+    ...
+
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
