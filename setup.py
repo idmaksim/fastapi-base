93fe87bf79c30a3ccfecbbd57592c6c968484bdd
idmaksim <i@dmaksim.ru>
2024-07-07 21:32:15 +0300

    upd version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi-base',
-    version='0.1',
+    version='1.0',
     entry_points={
         'console_scripts': [
             'fastapi-base = fastapi_base.main:main',
